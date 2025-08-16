54048e82488a9b4ba9c46f0517820fb46845175f
ashok <ashokgunji2017@gmail.com>
2025-08-16 18:41:29 +0530

    testing files

---
+++ A.java
@@ -0,0 +1,54 @@
+public class A{
+
+
+    public static void main(String[] args){
+
+
+    System.out.println("Hello World");
+
+   new A().m1();
+
+}
+
+
+  public void m1(){
+
+    System.out.println("m1() called....");
+
+}
+
+
+}
+==========================================================
+
+
+git commit: this command is used to commit our changes to the local repository.
+
+syntax: git commit -m 'first commit'
+
+
+
+Note: git local repository will be available in our local machine only.
+
+
+
+git log : this command is used to check commit history.
+
+
+
+==========================================================
+
+
+
+
+=======================================================
+
+
+
+
+
+
+
+
+
+
