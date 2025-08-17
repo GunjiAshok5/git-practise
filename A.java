68039e9c118db7616ed0de48681575bff6f0b6c2
ashok <ashokgunji2017@gmail.com>
2025-08-17 08:42:25 +0530

    Revert "git revert command checking"
    This reverts commit 9c76491337646aac554c6b4479091bbaab6ab42e.

--- A.java
+++ A.java
@@ -38,7 +38,7 @@ git log : this command is used to check commit history.
 
 ==========================================================
 
-checking only git revert command working or not
+
 
 
 =======================================================
